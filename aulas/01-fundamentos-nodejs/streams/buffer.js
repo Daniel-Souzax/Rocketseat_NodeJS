b030fa5ba159862cca504f6ff6f7ebf42bd654a0
Daniel S. Filho <danieldsfilho.dev@gmail.com>
2024-10-24 16:23:02 -0300

    aula

---
+++ aulas/01-fundamentos-nodejs/streams/buffer.js
@@ -0,0 +1,3 @@
+const buf = Buffer.from("snow")
+
+console.log(buf.toJSON());
---
+++ aulas/01-fundamentos-nodejs/streams/fake-upload-to-http-stream.js
@@ -0,0 +1,31 @@
+import { Readable, Writable, Transform } from 'node:stream'
+
+class OneToHundredStream extends Readable {
+    index = 1
+
+    _read() {
+        const i = this.index++
+        setTimeout(() => {
+
+            if (i > 5) {
+                this.push(null)
+            } else {
+                const buf = Buffer.from(String(i))
+                this.push(buf + ' ')
+            }
+
+        }, 500);
+
+    }
+}
+
+fetch('http://localhost:3334', {
+    method: 'POST',
+    duplex: 'half',
+    body: new OneToHundredStream()
+}).then(res => {
+    res.text().then(data => {
+        console.log(data);
+        
+    })
+})
